2a915b33cecc1539f7cbd9f0dc9d5d0379afae3b
thibaudcathala <thibaud.cathala@epitech.eu>
2023-02-27 18:33:22 +0100

    [ADD] macro for color

--- include/my_def.h
+++ include/my_def.h
@@ -8,15 +8,6 @@
 #ifndef MY_DEF_H_
     #define MY_DEF_H_
 
-    #define RESET_COLOR "\e[0m"
-    #define BOLD "\e[1m"
-    #define RED "\e[91m"
-    #define GREEN "\e[92m"
-    #define YELLOW "\e[93m"
-    #define BLUE "\e[94m"
-    #define MAGENTA "\e[95m"
-    #define CYAN "\e[96m"
-
     #define UNUSED(var) (void) (var)
 
     #define ADD_QUOTE(value) #value
