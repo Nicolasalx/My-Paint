2620ba1e5300bd5fcce2a013f73d2b567eb31de7
thibaudcathala <thibaud.cathala@epitech.eu>
2023-03-04 21:09:50 +0100

    [FIX] merge conflit

--- src/tool/pencil/render_pencil.c
+++ src/tool/pencil/render_pencil.c
@@ -13,26 +13,10 @@
 
 void render_pencil(void)
 {
-<<<<<<< HEAD
     sfVector2f circle_draw_pos = get_mouse_pos_on_sheet();
     circle_draw_pos.x -= pencil.radius;
     circle_draw_pos.y -= pencil.radius;
 
-=======
-    sfVector2f render_texture_pos = {
-        (mouse_pos.x - render_sheet_pos.x) / render_sheet_scale.x,
-        (mouse_pos.y - render_sheet_pos.y) / render_sheet_scale.y
-    };
-    sfVector2f render_texture_size = {
-        (float) render_sheet_resolution.x / render_sheet_scale.x,
-        (float) render_sheet_resolution.y / render_sheet_scale.y
-    };
-    sfVector2f circle_draw_pos = {
-        render_texture_pos.x + (pencil.radius / render_texture_size.x),
-        render_texture_pos.y + (pencil.radius / render_texture_size.y)
-    };
-    circle_draw_pos.x -= pencil.radius; circle_draw_pos.y -= pencil.radius;
->>>>>>> 6c84b05c86b1550bf35d2dac5a8c11c511be3f00
     sfCircleShape_setPosition(pencil.circle, circle_draw_pos);
     sfCircleShape_setRadius(pencil.circle, pencil.radius);
     sfCircleShape_setFillColor(pencil.circle, pencil.color);
