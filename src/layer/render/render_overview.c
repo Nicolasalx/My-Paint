061213887677e2a6b882cb571e554a100daa7cf9
thibaudcathala <thibaud.cathala@epitech.eu>
2023-03-07 17:50:17 +0100

    [ADD] outline to background

--- src/layer/render/render_overview.c
+++ src/layer/render/render_overview.c
@@ -13,4 +13,10 @@ void render_overview(sfRenderWindow *window)
     sfSprite_setPosition(GET_DATA(selected_layer, layer_t)->render_sprite, (sfVector2f) OVERVIEW_POS);
     sfSprite_setScale(GET_DATA(selected_layer, layer_t)->render_sprite, (sfVector2f) OVERVIEW_SCALE);
     sfRenderWindow_drawSprite(window, GET_DATA(selected_layer, layer_t)->render_sprite, NULL);
+    sfRectangleShape_setOutlineThickness(background, 6);
+    sfRectangleShape_setPosition(background, (sfVector2f) OVERVIEW_POS);
+    sfRectangleShape_setSize(background, render_sheet_resolution);
+    sfRectangleShape_setScale(background, (sfVector2f) OVERVIEW_SCALE);
+    sfRenderWindow_drawRectangleShape(window, background, NULL);
+    sfRectangleShape_setOutlineThickness(background, BACKGROUND_OUTLINE_THICKNESS);
 }
