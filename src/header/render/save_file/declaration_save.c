26ad72b8253ea53e3019132055ee7242eb0796fe
Nicolasalx <nicolas1.alexandre@epitech.eu>
2023-03-11 20:49:59 +0100

    [MOD] Front end save file

--- src/header/render/save_file/declaration_save.c
+++ src/header/render/save_file/declaration_save.c
@@ -24,11 +24,10 @@
     #define SIZE_BUTTON {120, 100}
 
 selection_extension_button_t selection_extension_button [] = {
-    {.color = COLOR_BG, .pos = {60, 80}, .size = {600, 100}, .content_text = "Entrez un nom de file", .pos_text = {220, 30}, .can_click = false},
-    {.color = COLOR_BG, .pos = {70, 300}, .size = SIZE_BUTTON, .content_text = "png", .pos_text = {105, 335}, .can_click = true},
-    {.color = COLOR_BG, .pos = {230, 300}, .size = SIZE_BUTTON, .content_text = "jpg", .pos_text = {265, 335}, .can_click = true},
-    {.color = COLOR_BG, .pos = {390, 300}, .size = SIZE_BUTTON, .content_text = "bmp", .pos_text = {425, 335}, .can_click = true},
-    {.color = COLOR_BG, .pos = {540, 300}, .size = SIZE_BUTTON, .content_text = "tga", .pos_text = {575, 335}, .can_click = true},
+    {.color = COLOR_BG, .pos = {60, 80}, .size = {600, 100}, .content_text = "Enter un nom de file", .pos_text = {220, 30}, .can_click = false},
+    {.color = COLOR_BG, .pos = {70, 300}, .size = SIZE_BUTTON, .content_text = "png", .pos_text = {100, 335}, .can_click = true},
+    {.color = COLOR_BG, .pos = {315, 300}, .size = SIZE_BUTTON, .content_text = "jpg", .pos_text = {340, 335}, .can_click = true},
+    {.color = COLOR_BG, .pos = {575, 300}, .size = SIZE_BUTTON, .content_text = "bmp", .pos_text = {600, 335}, .can_click = true},
     {.color = COLOR_BG, .pos = {225, 450}, .size = {300, 70}, .content_text = "Valider", .pos_text = {320, 470}, .validate = true},
 };
 
